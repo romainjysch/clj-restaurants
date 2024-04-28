fc4f6f58c4fb91666bd0f0d75f3aff3d8af48a87
Romain Jysch <rjh-network@pm.me>
2024-04-28 20:20:42 +0200

    feat(clj-restaurants): Update init.sql

--- .docker/mariadb/init.sql
+++ .docker/mariadb/init.sql
@@ -1,108 +1,113 @@
-DROP TABLE IF EXISTS RESTAURANTS;
-DROP TABLE IF EXISTS TYPES_GASTRONOMIQUES;
-DROP TABLE IF EXISTS VILLES;
-DROP TABLE IF EXISTS COMMENTAIRES;
-DROP TABLE IF EXISTS LIKES;
-DROP TABLE IF EXISTS NOTES;
-DROP TABLE IF EXISTS CRITERES_EVALUATION;
+create database restaurants;
+use restaurants;
 
-CREATE TABLE TYPES_GASTRONOMIQUES (
-    numero INT AUTO_INCREMENT NOT NULL,
-    libelle VARCHAR(100) NOT NULL UNIQUE,
-    description TEXT NOT NULL,
-    PRIMARY KEY (numero)
+--
+
+create table restaurants (
+    numero int auto_increment not null,
+    nom varchar(100) not null,
+    adresse varchar(100) not null,
+    description varchar(2000),
+    site_web varchar(100),
+    fk_type int not null,
+    fk_vill int not null,
+    primary key (numero)
 );
 
-CREATE TABLE VILLES (
-    numero INT AUTO_INCREMENT NOT NULL,
-    code_postal VARCHAR(100) NOT NULL,
-    nom_ville VARCHAR(100) NOT NULL,
-    PRIMARY KEY (numero)
+create table types_gastronomiques (
+    numero int auto_increment not null,
+    libelle varchar(100) not null unique,
+    description varchar(2000) not null,
+    primary key (numero)
 );
 
-CREATE TABLE RESTAURANTS (
-    numero INT AUTO_INCREMENT NOT NULL,
-    nom VARCHAR(100) NOT NULL,
-    adresse VARCHAR(100) NOT NULL,
-    description TEXT,
-    site_web VARCHAR(100),
-    fk_type INT NOT NULL,
-    fk_ville INT NOT NULL,
-    PRIMARY KEY (numero),
-    FOREIGN KEY (fk_type) REFERENCES TYPES_GASTRONOMIQUES(numero),
-    FOREIGN KEY (fk_ville) REFERENCES VILLES(numero)
+create table villes (
+    numero int auto_increment not null,
+    code_postal varchar(100) not null,
+    nom_ville varchar(100) not null,
+    primary key (numero)
 );
 
-CREATE TABLE COMMENTAIRES (
-    numero INT AUTO_INCREMENT NOT NULL,
-    date_eval DATE NOT NULL,
-    commentaire TEXT NOT NULL,
-    nom_utilisateur VARCHAR(100) NOT NULL,
-    fk_rest INT,
-    PRIMARY KEY (numero),
-    FOREIGN KEY (fk_rest) REFERENCES RESTAURANTS(numero)
+create table commentaires (
+    numero int auto_increment not null,
+    date_eval date not null,
+    commentaire varchar(2000) not null,
+    nom_utilisateur varchar(100) not null,
+    fk_rest int,
+    primary key (numero)
 );
 
-CREATE TABLE LIKES (
-    numero INT AUTO_INCREMENT NOT NULL,
-    appreciation CHAR(1) NOT NULL,
-    date_eval DATE NOT NULL,
-    adresse_ip VARCHAR(100) NOT NULL,
-    fk_rest INT NOT NULL,
-    PRIMARY KEY (numero),
-    FOREIGN KEY (fk_rest) REFERENCES RESTAURANTS(numero)
+create table likes (
+    numero int auto_increment not null,
+    appreciation char(1) not null,
+    date_eval date not null,
+    adresse_ip varchar(100) not null,
+    fk_rest int not null,
+    primary key (numero)
 );
 
-CREATE TABLE NOTES (
-    numero INT AUTO_INCREMENT NOT NULL,
-    note INT NOT NULL,
-    fk_comm INT NOT NULL,
-    fk_crit INT NOT NULL,
-    PRIMARY KEY (numero),
-    FOREIGN KEY (fk_comm) REFERENCES COMMENTAIRES(numero),
-    FOREIGN KEY (fk_crit) REFERENCES CRITERES_EVALUATION(numero)
+create table notes (
+    numero int auto_increment not null,
+    note decimal(3,0) not null,
+    fk_comm int not null,
+    fk_crit int not null,
+    primary key (numero)
 );
 
-CREATE TABLE CRITERES_EVALUATION (
-    numero INT AUTO_INCREMENT NOT NULL,
-    nom VARCHAR(100) NOT NULL UNIQUE,
-    description TEXT,
-    PRIMARY KEY (numero)
+create table criteres_evaluation (
+    numero int auto_increment not null,
+    nom varchar(100) not null unique,
+    description varchar(512),
+    primary key (numero)
 );
 
 --
 
-INSERT INTO TYPES_GASTRONOMIQUES(libelle, description) VALUES
-    ('Cuisine suisse', 'Cuisine classique et plats typiquement suisses'),
-    ('Restaurant gastronomique', 'Restaurant gastronomique de haut standing'),
-    ('Pizzeria', 'Pizzas et autres spécialités italiennes');
+alter table restaurants add constraint fk_rest_type foreign key (fk_type) references types_gastronomiques (numero);
+alter table restaurants add constraint fk_rest_vill foreign key (fk_vill) references villes (numero);
+alter table commentaires add constraint fk_comm_rest foreign key (fk_rest) references restaurants (numero);
+alter table notes add constraint fk_note_comm foreign key (fk_comm) references commentaires (numero);
+alter table notes add constraint fk_note_crit foreign key (fk_crit) references criteres_evaluation (numero);
+alter table likes add constraint fk_like_rest foreign key (fk_rest) references restaurants (numero);
 
-INSERT INTO CRITERES_EVALUATION(nom, description) VALUES
-    ('Service', 'Qualité du service'),
-    ('Cuisine', 'Qualité de la nourriture'),
-    ('Cadre', 'L''ambiance et la décoration sont-elles bonnes ?');
+--
 
-INSERT INTO VILLES(code_postal, nom_ville) VALUES
-    ('2000', 'Neuchâtel');
+insert into types_gastronomiques(libelle, description) values ('cuisine suisse', 'cuisine classique et plats typiquement suisses');
+insert into types_gastronomiques(libelle, description) values ('restaurant gastronomique', 'restaurant gastronomique de haut standing');
+insert into types_gastronomiques(libelle, description) values ('pizzeria', 'pizzas et autres spécialités italiennes');
 
-INSERT INTO RESTAURANTS(nom, adresse, description, site_web, fk_type, fk_ville) VALUES
-    ('Fleur-de-Lys', 'Rue du Bassin 10', 'Pizzeria au centre de Neuchâtel', 'http://www.pizzeria-neuchatel.ch', 3, 1),
-    ('La Maison du Prussien', 'Rue des Tunnels 11', 'Restaurant gastronomique renommé de Neuchâtel', 'www.hotel-prussien.ch', 2, 1);
+insert into criteres_evaluation(nom, description) values ('service', 'qualité du service');
+insert into criteres_evaluation(nom, description) values ('cuisine', 'qualité de la nourriture');
+insert into criteres_evaluation(nom, description) values ('cadre', 'l''ambiance et la décoration sont-elles bonnes ?');
 
-INSERT INTO COMMENTAIRES(date_eval, commentaire, nom_utilisateur, fk_rest) VALUES
-    (CURRENT_DATE, 'Génial !', 'Toto', 1),
-    (CURRENT_DATE, 'Très bon', 'Titi', 1),
-    (CURRENT_DATE, 'Un régal !', 'Dupont', 2),
-    (CURRENT_DATE, 'Rien à dire, le top !', 'Dupasquier', 2);
+insert into villes(code_postal, nom_ville) values ('2000', 'neuchâtel');
 
-INSERT INTO NOTES(note, fk_comm, fk_crit) VALUES
-    (4, 1, 1), (5, 1, 2), (4, 1, 3), (4, 2, 1), (4, 2, 2), (4, 2, 3),
-    (5, 3, 1), (5, 3, 2), (5, 3, 3), (5, 4, 1), (5, 4, 2), (5, 4, 3);
+insert into restaurants(nom, adresse, description, site_web, fk_type, fk_vill) values ('fleur-de-lys', 'rue du bassin 10', 'pizzeria au centre de neuchâtel', 'http://www.pizzeria-neuchatel.ch', 3, 1);
+insert into restaurants(nom, adresse, description, site_web, fk_type, fk_vill) values ('la maison du prussien', 'rue des tunnels 11', 'restaurant gastronomique renommé de neuchâtel', 'www.hotel-prussien.ch', 2, 1);
 
-INSERT INTO LIKES(appreciation, date_eval, adresse_ip, fk_rest) VALUES
-    ('T', CURRENT_DATE, '1.2.3.4', 1),
-    ('T', CURRENT_DATE, '1.2.3.5', 1),
-    ('F', CURRENT_DATE, '1.2.3.6', 1),
-    ('T', CURRENT_DATE, '1.2.3.7', 2),
-    ('T', CURRENT_DATE, '1.2.3.8', 2),
-    ('T', CURRENT_DATE, '1.2.3.9', 2);
+insert into commentaires(date_eval, commentaire, nom_utilisateur, fk_rest) values (curdate(), 'génial !', 'toto', 1);
+insert into commentaires(date_eval, commentaire, nom_utilisateur, fk_rest) values (curdate(), 'très bon', 'titi', 1);
+insert into commentaires(date_eval, commentaire, nom_utilisateur, fk_rest) values (curdate(), 'un régal !', 'dupont', 2);
+insert into commentaires(date_eval, commentaire, nom_utilisateur, fk_rest) values (curdate(), 'rien à dire, le top !', 'dupasquier', 2);
+
+insert into notes(note, fk_comm, fk_crit) values (4, 1, 1);
+insert into notes(note, fk_comm, fk_crit) values (5, 1, 2);
+insert into notes(note, fk_comm, fk_crit) values (4, 1, 3);
+insert into notes(note, fk_comm, fk_crit) values (4, 2, 1);
+insert into notes(note, fk_comm, fk_crit) values (4, 2, 2);
+insert into notes(note, fk_comm, fk_crit) values (4, 2, 3);
+insert into notes(note, fk_comm, fk_crit) values (5, 3, 1);
+insert into notes(note, fk_comm, fk_crit) values (5, 3, 2);
+insert into notes(note, fk_comm, fk_crit) values (5, 3, 3);
+insert into notes(note, fk_comm, fk_crit) values (5, 4, 1);
+insert into notes(note, fk_comm, fk_crit) values (5, 4, 2);
+insert into notes(note, fk_comm, fk_crit) values (5, 4, 3);
+
+insert into likes(appreciation, date_eval, adresse_ip, fk_rest) values ('t', curdate(), '1.2.3.4', 1);
+insert into likes(appreciation, date_eval, adresse_ip, fk_rest) values ('t', curdate(), '1.2.3.5', 1);
+insert into likes(appreciation, date_eval, adresse_ip, fk_rest) values ('f', curdate(), '1.2.3.6', 1);
+insert into likes(appreciation, date_eval, adresse_ip, fk_rest) values ('t', curdate(), '1.2.3.7', 2);
+insert into likes(appreciation, date_eval, adresse_ip, fk_rest) values ('t', curdate(), '1.2.3.8', 2);
+insert into likes(appreciation, date_eval, adresse_ip, fk_rest) values ('t', curdate(), '1.2.3.9', 2);
+
+commit;
